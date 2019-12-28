2c29e9705721845a5f6e129e44410b6a4c1d00ef
Shengliang Cai <cubercsl@163.com>
2019-12-28 17:19:34 +0800

    update index.php
    修复：
    - handles 有特殊字符时，查询会频繁失败。
    新增：
    - 使用 codeforces API 返回的 rank 来确定颜色，避免 codeforces 调整 rating 线。
    - 用户名根据 codeforces API 的返回结果自动修正大小写。
    - 使用本地搭建的Shields服务，不进行重定向。

--- index.php
+++ index.php
@@ -4,39 +4,31 @@
 // | PHP version 7                                                        |
 // +----------------------------------------------------------------------+
 // | Authors: abc1763613206 <abc1763613206@163.com>                       |
+// |          cubercsl <cubercsl@163.com>                                 |
 // +----------------------------------------------------------------------+
 //
 // $Id:$
 
-$user = $_GET["user"];
-$st = $_GET["style"];
-$st1 = $_GET["st"];
-$badgehd = "Location:https://img.shields.io/badge/";
-set_time_limit(600);
-$mainUrl = "http://codeforces.com/api/user.info?handles=*";
-$timeo = 0;
-do {   //发现真Unrated会返回OK....所以试下新操作
-  $ratingr = getData(str_replace("*", $user, $mainUrl));  //获取json数据并转换为数组
-  $timeo = $timeo +1;
-} while ($ratingr["status"] !== "OK" and $ratingr["comment"] !== "handles: User with handle ".$user." not found" and $timeo !== 12);
-$styleraw = "?longCache=true&style=*";
-if (isset($_GET["style"])) //是否使用自定义style
+const COLORS = [
+    "Legendary Grandmaster" => "ff0000.svg",
+    "International Grandmaster" => "ff0000.svg",
+    "Grandmaster" => "ff0000.svg",
+    "International Master" => "ff8c00.svg",
+    "Master" => "ff8c00.svg",
+    "Candidate Master" => "aa00aa.svg",
+    "Expert" => "0000ff.svg",
+    "Specialist" => "03a89e.svg",
+    "Pupil" => "008000.svg",
+    "Newbie" => "808080.svg",
+    "Unrated" => "000000.svg"
+];
+
+$badgeUrl = "http://localhost:8080/badge/"; // 使用本地搭建的Shields服务
+$mainUrl = "http://codeforces.com/api/user.info?handles=";
+
+function getdata($url)
 {
-    $style = str_replace("*", $st, $styleraw); //替换
-    
-} else {
-    $style = "?longCache=true&style=for-the-badge";  //默认是for-the-badge
-}
-if (isset($_GET["st"])) //自定义style缩写，不可重用
-{
-   switch($st1){
-     case "f1" : $style = "?longCache=true&style=flat";break;
-     case "f2" : $style = "?longCache=true&style=flat-square";break;
-     default: break;
-}
-    
-} 
-function getData($url) {
+    error_log("Request url: " . $url, 0);
     $headers = array();
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -48,49 +40,76 @@ function getData($url) {
     $result = curl_exec($ch);
     curl_close($ch);
     $result = json_decode($result, true);
-    return $result;  
+    return $result;
 }
-$ratstr = $ratingr["result"][0]["rating"];   //从数组中提取字符串rating
-$rating = intval($ratstr);    //转换为数字
-if ($rating >= 3000) {        //开始根据Rating判断段位~
-    $name = "-Legendary Grandmaster  ";
-    $color = "-red.svg";
-} elseif ($rating >= 2600) {
-    $name = "-International Grandmaster  ";
-    $color = "-red.svg";
-} elseif ($rating >= 2400) {
-    $name = "-Grandmaster  ";
-    $color = "-red.svg";
-} elseif ($rating >= 2300) {
-    $name = "-International master  ";
-    $color = "-ff8c00.svg";
-} elseif ($rating >= 2100) {
-    $name = "-Master  ";
-    $color = "-ff8c00.svg";
-} elseif ($rating >= 1900) {
-    $name = "-Candidate Master  ";
-    $color = "-aa00aa.svg";
-} elseif ($rating >= 1600) {
-    $name = "-Expert  ";
-    $color = "-00f.svg";
-} elseif ($rating >= 1400) {
-    $name = "-Specialist  ";
-    $color = "-03a89e.svg";
-} elseif ($rating >= 1200) {
-    $name = "-Pupil  ";
-    $color = "-008000.svg";
-} elseif ($rating !== 0) {   //这里可能翻车，有问题请及时反馈
-    $name = "-Newbie  ";
-    $color = "-808080.svg";
+
+function escapeuserrank($user)
+{
+    $result = str_replace("_", "__", $user);
+    $result = str_replace("-", "--", $result);
+    return $result;
+}
+
+if (!isset($_GET["user"]) || $_GET["user"] == "") {
+    http_response_code(404);
+    die("handles: Field should not be empty");
+}
+
+$user = trim($_GET["user"]);
+set_time_limit(600);
+$timeo = 0;
+do {   //发现真Unrated会返回OK....所以试下新操作
+    $response = getdata($mainUrl . rawurlencode($_GET["user"]));  //获取json数据并转换为数组
+    $timeo = $timeo + 1;
+} while (
+    $response["status"] !== "OK" and
+    !preg_match("/handles: User with handle .* not found/", $response["comment"]) and
+    $response["comment"] !== "handles: Field should not be empty" and
+    $timeo !== 12
+);
+
+if ($response["status"] !== "OK") {
+    http_response_code(404);
+    error_log("Query " . $user . " failed in " . $timeo . " time(s).", 0);
+    die($response["comment"]);
+}
+
+error_log("Query " . $user . " success in " . $timeo . " time(s).", 0);
+
+if (isset($_GET["style"])) { //是否使用自定义style
+    $style = "?longCache=true&style=" . $_GET["style"];
 } else {
-    $name = "-Unrated  ";
-    $color = "-black.svg";
+    $style = "?longCache=true&style=flat";
 }
-$style = $style. "&logo=Codeforces&link=https://codeforces.com/profile/" . $user; 
-$rawc1 = str_replace("_", "__", $user);
-$rawc2 = str_replace("-", "--", $rawc1);
-$rawr = $badgehd . $rawc2 . $name . $ratstr . $color . $style;
-header($rawr); //拼接并输出（修复下划线转义bug）
+if (isset($_GET["st"])) { //自定义style缩写，不可重用
+    switch ($_GET["st"]) {
+        case "f1":
+            $style = "?longCache=true&style=for-the-badge";
+            break;
+        case "f2":
+            $style = "?longCache=true&style=flat-square";
+            break;
+        default:
+            break;
+    }
+}
 
-?>
+$handle = $response["result"][0]["handle"];
+$rating = $response["result"][0]["rating"];
+$rank = $response["result"][0]["rank"] == null ?
+    "Unrated" :
+    ucwords($response["result"][0]["rank"]);
+$color = COLORS[$rank];
 
+$url = $badgeUrl .
+    escapeuserrank($handle) . "-" .
+    rawurlencode($rank . "  " . $rating . "-") .
+    $color .
+    $style .
+    "&logo=Codeforces" .
+    "&link=https://codeforces.com/profile/" . $handle;
+
+error_log("Request url: " . $url, 0);
+
+header("Content-Type: image/svg+xml");
+echo file_get_contents($url);
